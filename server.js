e3480c0db67044de2a8ece238f2eb51cfe510ffe
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-05-21 19:08:06 -0500

    adding browser opening functionality

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // server.js
 
 var express = require('express'),
+	open = require('open'),
 	app = express();
 
 app.set('port', 8000);
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res) {
 	res.send('index.html');
@@ -13,3 +15,5 @@ app.listen(app.get('port'), function() {
 	console.log('Listening on port ' + app.get('port'));
 });
 
+
+open('http://localhost:8000');
